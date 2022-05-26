c2d27730136329827e90507d490fe74e64e34deb
mattsiis <35969076+mattsiis@users.noreply.github.com>
2022-05-26 01:59:57 -0400

    feature/react-router

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { 
+import {
   ApolloProvider,
   InMemoryCache,
   ApolloClient
@@ -10,7 +10,15 @@ import {
 import Header from './components/Header';
 import Footer from './components/Footer';
 
+import { BrowserRouter as Router, Route, Routes, } from 'react-router-dom';
+
 import Home from './pages/Home';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
+
 
 const client = new ApolloClient({
   uri: '/graphql',
@@ -19,15 +27,27 @@ const client = new ApolloClient({
 
 function App() {
   return (
-    <ApolloProvider client={client}> 
+    <ApolloProvider client={client}>
+  
       <div className='flex-column justify-flex-start min-100-vh'>
-        <Header />
-        <div className='container'>
-          <Home />
-        </div>
+        <Router>
+          <Header></Header>
+          <div className='container'>
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/login" element={<Login />} />
+              <Route exact path="/signup" element={<Signup />} />
+              <Route exact path="/profile/:username" element={<Profile />} />
+              <Route exact path="/thought/:id" element={<SingleThought />} />
+
+              <Route path="*" element={<NoMatch />} />
+            </Routes>
+          </div>
+        </Router>
         <Footer />
       </div>
-    </ApolloProvider>
+
+    </ApolloProvider >
   );
 }
 
